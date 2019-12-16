b4f42754980342728708c170edd4cb6bc02a6a8e
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-16 00:37:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word 
- sentence="Hello there, and how are you?"
+def reverse_each_word (sentence)
+    "Hello there, and how are you?"
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
   reverse_each_word
